fd587c32bc8f90c777a60d2d96a86f12a784b87e
eyakub <eyakubsorkar@gmail.com>
2020-12-31 04:36:55 +0600

    fiexd the nav for mobile view

--- js/index.js
+++ js/index.js
@@ -2,21 +2,26 @@
 const navOpen = document.querySelector(".nav__hamburger");
 const navClose = document.querySelector(".close__toggle");
 const menu = document.querySelector(".nav__menu");
+const scrollLink = document.querySelectorAll(".scroll-link");
 const navContainer = document.querySelector(".nav__menu");
 
 navOpen.addEventListener("click", () => {
+    console.log("Clicked nav")
     menu.classList.add("open");
     document.body.classList.add("active");
     navContainer.style.left = "0";
     navContainer.style.width = "30rem";
 });
 
-navClose.addEventListener('click', () => {
-    menu.classList.remove('open');
-    document.body.classList.remove('active');
+navClose.addEventListener("click", () => {
+    menu.classList.remove("open");
+    document.body.classList.remove("active");
     navContainer.style.left = "-30rem";
     navContainer.style.width = "0";
 });
+
+// glide init
+// new Glide('.glide').mount();
 
 // AOS init
 AOS.init();
