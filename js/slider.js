3bd636f6e1824a0e8ee2e037d6f7702d905389c9
eyakub <eyakubsorkar@gmail.com>
2021-01-03 03:00:16 +0600

    glide on latest products working

--- js/slider.js
+++ js/slider.js
@@ -2,11 +2,12 @@
  * Glide js carousel
  */
 const slider1 = document.getElementById('glide_1');
+const slider2 = document.getElementById('glide_2');
 
 /**
  * Hero
  */
-if(slide1){
+if (slider1) {
     new Glide(slider1, {
         type: "carousel",
         startAt: 0,
@@ -15,5 +16,27 @@ if(slide1){
         perView: 1,
         animationDuration: 800,
         animationTimingFunc: 'linear',
+    }).mount();
+}
+
+/**
+ * latest products
+ */
+if (slider2) {
+    new Glide(slider2, {
+        type: "carousel",
+        startAt: 0,
+        hoverpause: true,
+        perView: 4,
+        animationDuration: 800,
+        animationTimingFunc: 'cubic-bezier(.165, .840, .44, .1)',
+        breakpoints: {
+            1200: {
+                perView: 3
+            },
+            768: {
+                perView: 2,
+            },
+        }
     }).mount();
 }
